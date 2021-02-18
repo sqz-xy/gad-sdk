d6c42c374effd2c94bee80c639bf101aafa5aba9
Stan McCarthy <smccarthy@oxts.com>
2021-02-18 10:20:58 +0000

    Changes to the separated core repo.

--- oxts-sdk-core/cmake/oxts-sdk-core-config.cmake
+++ oxts-sdk-core/cmake/oxts-sdk-core-config.cmake
@@ -1,3 +1,3 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${OXTS_SDK_LIB_NAME}-targets.cmake)
-get_filename_component(${PROJECT_NAME}_INCLUDE_DIRS "${SELF_DIR}/../../include/${PROJECT_NAME}" ABSOLUTE)
+include(${SELF_DIR}/oxts-sdk-core-targets.cmake)
+get_filename_component(oxts-sdk-core_INCLUDE_DIRS "${SELF_DIR}/../../include/${PROJECT_NAME}" ABSOLUTE)

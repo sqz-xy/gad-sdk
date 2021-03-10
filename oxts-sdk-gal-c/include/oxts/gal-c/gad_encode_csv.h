e902e115e4027966fe899f7bd72d9bd953276ebc
Stan McCarthy <smccarthy@oxts.com>
2021-03-10 15:39:38 +0000

    gal-c .gad file writing refactor, splitting encoding and writing to file.

--- oxts-sdk-gal-c/include/oxts/gal-c/gad_encode_csv.h
+++ oxts-sdk-gal-c/include/oxts/gal-c/gad_encode_csv.h
@@ -1,5 +1,5 @@
-#ifndef GAD_CSV_H
-#define GAD_CSV_H
+#ifndef GAD_ENCODE_CSV_H
+#define GAD_ENCODE_CSV_H
 
 #include "oxts/gal-c/gad_defines.h"
 #include "oxts/gal-c/gad_struct.h"
@@ -10,28 +10,31 @@ extern "C"
 {
 #endif // __cplusplus
 
-/*
-    associated the file handler to the gad engine file handler and prints the header to the csv gad file
-
-    params:
-    * file_ptr - pointer to file you're writing csv to 
-*/
+/** Opens the .gad file and prints the header to the csv gad file.
+ * @param file_ptr Pointer to the file to be written to 
+ */
 int init_gad_output_file(FILE* file_ptr);
 
-
-int encode_gen_3d_to_csv(unsigned char * out_string, int * offset, GEN_3D* gen_3d_data);
-
-/*
-    writes the generic aiding data in data to file at file_ptr
-
-    params:
-    * file_ptr - pointer to file you're writing csv to 
-    * data - generic aiding data being written
+/** Encode data from GEN_3D to csv format for writing to file.
+ * @param out_string Char array to be populated with GEN_3D data
+ * @param offset Current offset into output buffer
+ * @param gen_3d_data Data to be written to the output string.
+ */
+int encode_gen_3d_to_csv(char * out_string, int * offset, GEN_3D* gen_3d_data);
+/** Encode data from GEN_AIDING_DATA to csv format for writing to file. 
+ * @param out_string Char array to be populated with GEN_AIDING_DATA.
+ * @param data GEN_AIDING_DATA to be written to the output string.
 */
+int encode_gad_to_csv(char * out_string, GEN_AIDING_DATA* data);
+
+/** Writes the generic aiding data in data to file at file_ptr
+ * @param file_ptr Pointer to the file to write data to. 
+ * @param data Generic aiding data to be written.
+ */
 int write_gad_to_csv(FILE* file_ptr, GEN_AIDING_DATA* data);
 
 
 #ifdef __cplusplus
 }
 #endif // __cplusplus
-#endif // GAD_CSV_H
+#endif // GAD_ENCODE_CSV_H

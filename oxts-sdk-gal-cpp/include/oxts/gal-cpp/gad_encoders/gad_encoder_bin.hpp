6366beb373c9ceb043c11d90c14be26ebbc282f5
Stan McCarthy <smccarthy@oxts.com>
2021-03-11 15:49:26 +0000

    Comments on gad_encoder_bin struct, and moved CCOM_MSG variable to private.

--- oxts-sdk-gal-cpp/include/oxts/gal-cpp/gad_encoders/gad_encoder_bin.hpp
+++ oxts-sdk-gal-cpp/include/oxts/gal-cpp/gad_encoders/gad_encoder_bin.hpp
@@ -36,14 +36,17 @@ private:
     return encode_gen_aid(&genaid, this->buffer, this->buffer_size,&this->gad_size);
   }
 
+  CCOM_MSG ccom_gad;
 public:
-
+  /** Constructor */
   GadEncoderBin()
   {
     this->buffer_offset = 0;
     this->gad_size = 0;
   }
-
+  /** Override of EncodePacket function to encode data from Gad class to a 
+   *  binary CCom packet for transmission to an OxTS INS.
+   */
   void EncodePacket(Gad& g) override
   {
     // Encode Gad
@@ -53,19 +56,22 @@ public:
     this->ccom_gad.type = (CCOM_PKT_GEN_AIDING);
     BuildCComPkt(&this->ccom_gad, this->buffer, this->gad_size);
   }
-
+  /** Override of the GetPacket function to retrieve the CCom message to be 
+   *  sent to an INS.
+   */
   virtual unsigned char * GetPacket() override
   {
     return this->ccom_gad.msg;
   }
-
+  /** Override of the GetPacketSize function, to retrieve the size of the 
+   * generic aiding packet (wrapped in CCom) to be transmitted to an INS.
+   */
   virtual std::size_t GetPacketSize() override
   {
     return this->ccom_gad.msg_len;
   }
 
 
-  CCOM_MSG ccom_gad;
 };
 
 }

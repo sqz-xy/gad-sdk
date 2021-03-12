cf72be43cf3882e696d3ae33980ef1ef11067428
Stan McCarthy <smccarthy@oxts.com>
2021-03-12 11:32:48 +0000

    Option to disable Boost added.

--- oxts-sdk-gal-cpp/include/oxts/gal-cpp/gad_output/gad_output_udp.hpp
+++ oxts-sdk-gal-cpp/include/oxts/gal-cpp/gad_output/gad_output_udp.hpp
@@ -1,17 +1,21 @@
 #ifndef GAD_OUTPUT_UDP_HPP
 #define GAD_OUTPUT_UDP_HPP
 /** @file gad_output_udp.hpp */
-
+#include "oxts/gal-cpp/config.h"
 #include "oxts/gal-cpp/gad_output/gad_output.hpp"
+
+#ifndef OXTS_SDK_DISABLE_BOOST
 #include "oxts/gal-cpp/udp_server_client.h"
+#endif
 
 namespace OxTS
 {
-
+#ifndef OXTS_SDK_DISABLE_BOOST
 class GadOutputUdp : public GadOutput
 {
 private:
   /** Server to receive the GAD. */
+
   std::shared_ptr<networking_udp::server> udpServer_;
   /** Port on the INS to recieve GAD. */
   short unitGaPort = 50485;
@@ -31,6 +35,25 @@ public:
     udpServer_->send(packet, packet_size);
   }
 };
+#else
+class GadOutputUdp : public GadOutput
+{
+private:
+
+  short unitGaPort = 50485;
+public:
+  /** Constructor */
+  GadOutputUdp(std::string ip){  }
+  /** Send the packet over UDP to the configured endpoint.
+   * @param packet Encoded data to be sent.
+   * @param packet_size Size of the packet in bytes.
+   */
+  void virtual OutputPacket(unsigned char * packet, int packet_size) override
+  {
+    // Send Packet to unit endpoint (Unit IP, Port 50485)
+  }
+};
+#endif // OXTS_SDK_DISABLE_BOOST
 
 } // OxTS
 

e37045f116b32de0aec91a01e02fd882f8d3d602
Stan McCarthy <smccarthy@oxts.com>
2021-03-31 14:42:26 +0100

    Added my-first-gad example code.

---
+++ examples/gal/my-first-gad.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+#include "oxts/gal-cpp/gad.hpp"
+#include "oxts/gal-cpp/gad_handler.hpp"
+
+#ifdef _WIN32
+#include <windows.h>
+#elif __linux__
+#include <unistd.h>
+#endif
+// This is a basic cross-platform sleep function
+namespace OxTS
+{
+void Sleep(int sleepMs)
+{
+#ifdef __linux__
+    usleep(sleepMs * 1000);   // usleep takes sleep time in us (1 millionth of a second)
+#endif
+#ifdef _WIN32
+    Sleep(sleepMs);    // Sleep takes sleep time in ms.
+#endif
+}
+} // OxTS
+
+
+int main(int argc, char * argv[])
+{
+  std::string unit_ip   = argv[1]; // Unit to send GAD to
+  int num_packets       = 100;     // Number of packets to send.
+
+  // Construct the velocity aiding with stream ID 130.
+  OxTS::GadVelocity gv = OxTS::GadVelocity(130);
+
+  // Set the required data
+  gv.SetVelNeu(0.0,0.0,0.0);
+  gv.SetVelNeuVar(0.1,0.1,0.1);
+  gv.SetTimeVoid();
+  gv.SetAidingLeverArmFixed(0.0,0.0,1.0);
+  gv.SetAidingLeverArmVar(0.01,0.01,0.01);
+
+  // Initialise the handler
+  OxTS::GadHandler gh = OxTS::GadHandler();
+  gh.SetEncoderToBin();
+  gh.SetOutputModeToUdp(unit_ip);
+
+  // Send the packets
+  for (int i = 0; i < num_packets; ++i)
+  {
+    gh.SendPacket(gv);
+    OxTS::Sleep(100);
+  }
+
+  return 0;
+}

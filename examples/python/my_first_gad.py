b6a0a65e9751c5bbc5f6bc4c363f3e1ef012b656
Stan McCarthy <smccarthy@oxts.com>
2021-10-04 18:09:13 +0100

    Adding example of full covariance matrix to python example, in position.

--- examples/python/my_first_gad.py
+++ examples/python/my_first_gad.py
@@ -5,9 +5,16 @@ gh = oxts_sdk.GadHandler()
 gh.set_encoder_to_csv()
 gh.set_output_mode_to_file("my_first.gad")
 
+gp = oxts_sdk.GadPosition(122)
+gp.pos_local = [0.0,0.0,0.0]
+gp.pos_local_var = [0.1,0.1,0.1,0.01,0.02,0.04]
+gp.time_gps = [10, 0]
+gp.aiding_lever_arm_fixed = [0.0,0.0,1.0]
+gp.aiding_lever_arm_var = [0.01,0.01,0.01]
+
 gv = oxts_sdk.GadVelocity(123)
 gv.vel_neu = [0.0,0.0,0.0]
-gv.vel_neu_var = [0.1,0.1,0.1]
+gv.vel_neu_var = [0.1,0.2,0.3]
 gv.time_gps = [10, 0]
 gv.aiding_lever_arm_fixed = [0.0,0.0,1.0]
 gv.aiding_lever_arm_var = [0.01,0.01,0.01]
@@ -22,7 +29,9 @@ gs.aiding_lever_arm_var = [0.01,0.01,0.01]
 num_packets = 100
 # Send the packets
 for i in range(0, num_packets):
+    gh.send_packet(gp)
     gh.send_packet(gv)
     gh.send_packet(gs)
+    gp.time_gps = [10, i]
     gv.time_gps = [10, i]
     gs.time_gps = [10, i]

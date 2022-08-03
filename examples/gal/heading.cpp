6b931f59d24d20bb7d3ed248c39ccebeb873331c
Scott Logan <SLOGAN@oxts.com>
2022-08-03 14:16:40 +0100

    Minor style issues

--- examples/gal/heading.cpp
+++ examples/gal/heading.cpp
@@ -23,7 +23,7 @@ namespace OxTS
 	}
 } // OxTS
 
-enum OUTPUT_TYPE
+enum class OUTPUT_TYPE
 {
 	UDP = 0,
 	CSV = 1
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
 	int num_packets = 30; // Total number of packets to send
 	std::string unit_ip = "192.168.25.22"; // Unit to send GAD to
 	std::string file_out = "out.gad";    // File to send GAD to
-	int output_type = OUTPUT_TYPE::CSV;   // Set output to UDP or CSV
+	OUTPUT_TYPE output_type = OUTPUT_TYPE::CSV;   // Set output to UDP or CSV
 
 	//Get from user input if provided
 	if (argc > 1)
